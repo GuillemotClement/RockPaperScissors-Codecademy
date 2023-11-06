8f095e23593d1c127ad614efbf4faceaa56b69f8
GuillemotClement <guillemot.clement@protonmail.com>
2023-11-06 15:51:01 +0100

    Ajout de la fonction de vérification de input user

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const getUserChoice = userInput => userInput.toLowerCase();
+let userInput = 'sCissors';
+//fonction qui récupère le choix de l'utilisateur et le passe en minuscule
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase(); //prévoir de passer par un appel de fonction pour lower la saisis user
 
-console.log(getUserChoice('RERE'));
+    //Verification de la saisis de l'utilisateur
+    if(userInput === 'rock'){
+        return userInput;
+    }else if(userInput === 'paper'){
+        return userInput;
+    }else if(userInput === 'scissors'){
+        return userInput;
+    }else{
+        return 'User input error';
+    };
+};
+
+console.log(getUserChoice(userInput));
+
+
